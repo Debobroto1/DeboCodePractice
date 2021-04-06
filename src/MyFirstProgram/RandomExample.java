3aab03d0338da157f6d1347d5128858a5a15bca5
debo <devaryan.sen@gmail.com>
2021-04-06 21:43:09 +0530

    updated Random example

--- src/MyFirstProgram/RandomExample.java
+++ src/MyFirstProgram/RandomExample.java
@@ -8,10 +8,11 @@ public class RandomExample {
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		int x = random.nextInt(6) + 1;// Dice
-		// double y = random.nextDouble();
-		// boolean z = random.nextBoolean();
+		int x = random.nextInt(6) + 1;// Dice //+1 means the number will start from 1 and not 0
+		double y = random.nextDouble(); //shows number between 01 and 1
+		boolean z=random.nextBoolean();
 		JOptionPane.showMessageDialog(null, x);
-		//System.out.println(x);
-	}
+		System.out.println(y);
+		System.out.println(z);
+}
 }
